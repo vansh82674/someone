4bfc881ed405f6753a41f71e01d531346efdacb6
Vansh <nexusop82@gmail.com>
2026-09-01 16:26:18 +0530

    feat: complete end-to-end hello world pipeline with socket.io and prisma

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,28 +1,40 @@
 'use client'
-import { useEffect } from "react";
-import { io } from "socket.io-client";
+import { useEffect, useRef } from "react";
+import { io, Socket } from "socket.io-client";
 import { Button } from "@/components/ui/button";
 export default function Home() {
+  // Store the socket connectioin in a red so our button can use it
+  const socketRef = useRef<Socket | null>(null)
 
   useEffect(() => {
     // Dial the backend
-    const socket = io('http://localhost:8080')
+    socketRef.current = io('http://localhost:8081')
 
-    socket.on('connect', () => {
+    socketRef.current.on('connect', () => {
       console.log("Successfully connected to backend!")
     })
 
+    // Listen for the backend's reply
+    socketRef.current.on("queue_joined", (data) => {
+      alert(data.message) // should pop up "You are officially in the queue"
+    })
 
     // Cleanup connnection when the user leaves page
     return () => {
-      socket.disconnect()
+      socketRef.current?.disconnect()
     }
 
   }, [])
+
+
+  const handleJoinQueue = () => {
+    // Send the event to the backend
+    socketRef.current?.emit("join_queue", { username: "Nexus" })
+  }
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-4">
       <h1 className="font-heading text-4xl font-bold">SOMEONE</h1>
-      <Button className='bg-brand-violet text-white font-sans'>Start Chatting</Button>
+      <Button className='bg-brand-violet text-white font-sans' onClick={handleJoinQueue}>Start Chatting</Button>
     </div>
 
   );
